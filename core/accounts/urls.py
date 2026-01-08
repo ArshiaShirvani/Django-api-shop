1e0745138bfcf8cfe6dc679befdd2bc53e19a9dd
Arshia <arshiashirvani.f1385@gmail.com>
2026-01-08 19:49:05 +0330

    Update urls.py

---
+++ core/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('auth/', views.RequestOtpView.as_view(), name='otp-request'),
+    path('auth/verify/', views.VerifyOtpView.as_view(), name='otp-verify'),
+    path("token/refresh/",TokenRefreshView.as_view(),name="refresh-token"),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+]
