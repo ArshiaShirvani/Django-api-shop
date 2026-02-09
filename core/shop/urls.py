f0cb76c82d3a6ce9050f5ebc49d6ade1b325f952
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-10 02:21:27 +0330

    Update urls.py

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -5,4 +5,9 @@ app_name = "shop"
 
 urlpatterns = [
     path("test/", views.TestApiView.as_view(), name="test"),
+    path("list/",views.ProductListApiView.as_view(),name="product-list"),
+    path("detail/<slug:slug>/", views.ProductDetailApiView.as_view(),name="product-detail"),
+    # path("detail/<slug:slug>/",views.ProductDetailApiView.as_view(),name="product-detail"),
+    # path('categories/', CategoryListAPI.as_view()),
+    # path('categories/<slug:slug>/', CategoryDetailAPI.as_view()),
 ]
