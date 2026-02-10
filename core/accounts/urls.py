538d3a4b3a59084f6d0300c0f322dfcec765ac64
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-10 21:00:21 +0330

    Update urls.py

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('auth/', views.RequestOtpView.as_view(), name='otp-request'),
     path('auth/verify/', views.VerifyOtpView.as_view(), name='otp-verify'),
     path("token/refresh/",TokenRefreshView.as_view(),name="refresh-token"),
+    path('information/', views.CurrentUserApiView.as_view(), name='information'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
