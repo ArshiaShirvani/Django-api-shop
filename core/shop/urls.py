1bb826654160f38b5607e19de01c1c942703a6b9
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-15 00:25:06 +0330

    Update urls.py

--- core/shop/urls.py
+++ core/shop/urls.py
@@ -1,12 +1,12 @@
 from . import views
-from django.urls import path
+from django.urls import path,re_path
 
 app_name = "shop"
 
 urlpatterns = [
     path("test", views.TestApiView.as_view(), name="test"),
     path("products",views.ProductListApiView.as_view(),name="product-list"),
-    path("products/<slug:slug>/", views.ProductDetailApiView.as_view(),name="product-detail"),
+    re_path(r"^products/(?P<slug>.+)/$", views.ProductDetailApiView.as_view(), name="product-detail"),
     # path("detail/<slug:slug>/",views.ProductDetailApiView.as_view(),name="product-detail"),
     # path('categories/', CategoryListAPI.as_view()),
     # path('categories/<slug:slug>/', CategoryDetailAPI.as_view()),
