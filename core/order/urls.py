28a4d67de76e112a49609058828bb384368eae08
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-19 21:11:13 +0330

    Update urls.py

--- core/order/urls.py
+++ core/order/urls.py
@@ -6,5 +6,7 @@ app_name = "order"
 
 
 urlpatterns = [
-    
+    path("create/", views.OrderCreateApiView.as_view(), name="order-create"),
+    path("<int:pk>/", views.OrderDetailApiView.as_view(), name="order-detail"),
+    path("list/", views.UserOrdersListApiView.as_view(), name="user-orders"),
 ]
