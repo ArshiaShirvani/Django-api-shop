b5e8b6d3e6a7e877187c0f01efd22cf12d128215
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-19 21:11:04 +0330

    Update models.py

--- core/cart/models.py
+++ core/cart/models.py
@@ -3,28 +3,37 @@ from accounts.models import User
 from shop.models import ProductVariant
 
 class Cart(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="cart")
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="cart",verbose_name="کاربر")
     
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ایجاد')
+    updated_date = models.DateTimeField(auto_now=True,verbose_name='تاریخ آخرین بروزرسانی')
     
     def __str__(self):
         return self.user.phone_number
     
     def total_price(self):
         pass
+    
+    class Meta:
+        verbose_name = 'سبد خرید'
+        verbose_name_plural = 'سبد های خرید'
         
     
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name="items")
-    variant = models.ForeignKey(ProductVariant,on_delete=models.CASCADE,related_name="cart_items")
-    quantity = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey(Cart,on_delete=models.CASCADE,related_name="items",verbose_name="سبد خرید")
+    variant = models.ForeignKey(ProductVariant,on_delete=models.CASCADE,related_name="cart_items",verbose_name="محصول")
+    quantity = models.PositiveIntegerField(default=1,verbose_name="تعداد")
     
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ایجاد')
+    updated_date = models.DateTimeField(auto_now=True,verbose_name='تاریخ آخرین بروزرسانی')
     
     class Meta:
         unique_together = ('cart', 'variant')
+        verbose_name = 'آیتم سبد خرید'
+        verbose_name_plural = 'آیتم های سبد خرید'
+        
+    
+        
     
     @property
     def price(self):
