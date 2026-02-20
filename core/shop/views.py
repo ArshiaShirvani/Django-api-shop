23aff103d135dc37b0cf564c3be5bf8e2f9c0b4b
Arshia <arshiashirvani.f1385@gmail.com>
2026-02-20 21:14:00 +0330

    Update views.py

--- core/shop/views.py
+++ core/shop/views.py
@@ -50,7 +50,7 @@ class TestApiView(APIView):
 
 
 class ProductPagination(PageNumberPagination):
-    page_size = 12
+    page_size = 18
     page_size_query_param = "page_size"
     max_page_size = 50
 
