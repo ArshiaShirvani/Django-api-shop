dcfb71bc0a75912ec0e10f5b44a206f9a2c4635a
Arshia <arshiashirvani.f1385@gmail.com>
2026-08-17 23:41:38 +0330

    Create views.py

---
+++ core/review/views.py
@@ -0,0 +1,35 @@
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from .models import Review
+from .serializers import ReviewSerializer
+
+
+class ReviewListCreateView(generics.ListCreateAPIView):
+    serializer_class = ReviewSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+
+        return [IsAuthenticated()]
+
+    def get_queryset(self):
+        product_id = self.kwargs.get("product_id")
+
+        return Review.objects.filter(
+            product_id=product_id
+        ).select_related("user", "product")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class ReviewUpdateView(generics.UpdateAPIView):
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Review.objects.filter(
+            user=self.request.user
+        ).select_related("user", "product")
