92b60ef55bcdf084e8b43e4da3cd913dfda7376d
Arshia <arshiashirvani.f1385@gmail.com>
2026-09-02 14:30:58 +0330

    Update urls.py

--- core/payment/urls.py
+++ core/payment/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PaymentRequestApiView, PaymentVerifyApiView
+
 
 app_name = "payment"
 
 urlpatterns = [
-    path("request/<int:order_id>/", PaymentRequestApiView.as_view(), name="request"),
-    path("verify/", PaymentVerifyApiView.as_view(), name="verify"),
+    # path("request/<int:order_id>/", PaymentRequestApiView.as_view(), name="request"),
+    # path("verify/", PaymentVerifyApiView.as_view(), name="verify"),
 ]
